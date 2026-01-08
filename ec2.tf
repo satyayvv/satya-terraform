e6f9e4560e6891693156b5dc58bdbb17ab6c0325
dsomas91 <dsomas91@hotmail.com>
2026-01-08 17:52:04 +0000

    add security group to VM

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "example" {
   ami           = var.satya-ami
   instance_type = var.vm-size
-
+#attacing security group
+vpc_security_group_ids = [ aws_security_group.allow_tls.id ]
   tags = {
     Name = var.vm-name
   }
